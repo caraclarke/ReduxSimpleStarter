91ebb3f30a8b586ca09e21969aabc884ef5fbddc
caraclarke <cara.clarke8@gmail.com>
2016-07-18 12:57:40 -0400

    Added fetch weather action, added redux to SearchBar
    created fetch weather action
    added redux to searchbar
    preparing to make/test ajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,22 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
+import axios from 'axios';
 
 const API_KEY = process.env.WEATHER_KEY;
+// es6 template strings - make sure to use backticks
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// call action creator to make ajax request
+
+// define var, assign string and export var
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axois.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
