7073a05a4efabfa144b8f09593e60b231279147c
caraclarke <cara.clarke8@gmail.com>
2016-07-18 10:09:13 -0400

    input correctly displaying in search bar
    bound "this" in constructor function
    no longer getting error

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // value of input mapped to this.state.term
+    this.state = { term: '' }
+
+    // bind context of onInputChange
+    /*
+      (after equal sign) **this** refers to instance of SearchBar has a function called onInputChange
+
+      bind that function to **this**(SearchBar) and replace onInputChange with new bound instance of function
+    */
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
