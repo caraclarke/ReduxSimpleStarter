c5ca21e0d005a64481b2963c5e8d20bcf1e21c98
caraclarke <cara.clarke8@gmail.com>
2016-07-18 10:41:43 -0400

    Added onFormSubmit function

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -21,9 +21,15 @@ export default class SearchBar extends Component {
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    // need to go fetch weather data
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
